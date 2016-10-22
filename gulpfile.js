b78e31e476ef142a53c494be2c5ab5ad7ea22fa1
Kleiton Silva Souza de Azevedo <macuser@admins-MacBook-Pro.local>
2016-10-22 13:19:56 -0400

    add env

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// add Modulos
 var gulp          = require('gulp'),
     jade          = require('gulp-jade'),
     autoPrefixer  = require('gulp-autoprefixer'),
@@ -5,6 +6,7 @@ var gulp          = require('gulp'),
     sass          = require('gulp-sass'),
     clearCss      = require('gulp-clean-css'),
     gulpIf        = require('gulp-if'),
+    env           = process.env.NODE_ENV || 'dev',
     uglify        = require('gulp-uglify'),
     browserSync   = require('browser-sync').create(),
     reload        = browserSync.reload;
